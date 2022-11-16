bfa1f05831065f296df4b8ba4e2e5e8c0eb4ffea
Gabriel Rodrigo <pdktche-pb@live.com>
2022-11-16 20:37:18 -0300

    Foi feito o exercicio 2.1.2

---
+++ 2.1.2.js
@@ -0,0 +1,16 @@
+let caminho5km = true
+let corro2km = true
+if(caminho5km && corro2km){
+    console.log("Passo o dia bem.")
+}
+
+let levantocedo = 5
+if(levantocedo && levantocedo < 8){
+    console.log("Saio Tranquilo.")
+}
+
+let trabalhoterminado = false
+let compromisso = true
+if(trabalhoterminado == false || compromisso == true){
+    console.log("Saio tarde.")
+}
