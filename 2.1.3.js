2b22a41f3b7af9ef13a31405d6939a2183002743
Gabriel Rodrigo <pdktche-pb@live.com>
2022-11-16 22:21:19 -0300

    Foi feito o exercicio 2.1.3

---
+++ 2.1.3.js
@@ -0,0 +1,7 @@
+//1.1
+var primo = 5
+if(primo/primo == 1 || primo / primo == primo){
+    console.log("Este numero é primo.")
+}
+//1.2
+var hora = 1; var minutos = 0; var hora = hora == 0 ? minutos = hora * 60 : minutos = 60;
